b991a3f7ee790642486af4988e988b3a1f7e70e0
walsker <walskerw@gmail.com>
2019-12-28 02:19:40 -0500

    Disable new applications

--- client/src/pages/application/index.js
+++ client/src/pages/application/index.js
@@ -114,7 +114,7 @@ const Application = () => {
             <Route
               path={`${path}/form`}
               render={() =>
-                appStatus === 'unstarted' || appStatus === 'submitted' || appStatus === 'unsubmitted' ? (
+                appStatus === 'submitted' || appStatus === 'unsubmitted' ? (
                   <ApplicationForm applicationForm={applicationForm} setApplication={setApplication} />
                 ) : (
                   <Redirect to={`${path}/status`} />
